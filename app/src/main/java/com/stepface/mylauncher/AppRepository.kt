76bd11094c6d4c7c5cf5f363f43c169e9f4acd1a
Saurav Patel <saurav123patel@gmail.com>
2019-07-07 21:04:04 +0530

    Initial commit

---
+++ app/src/main/java/com/stepface/mylauncher/AppRepository.kt
@@ -0,0 +1,32 @@
+@file:Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+
+package com.stepface.mylauncher
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+
+class AppRepository(context: Context, appName: String) {
+
+    private val packageManager: PackageManager = context.packageManager
+    private val applicationInfo: ApplicationInfo = context.applicationInfo
+    private val settingsApp = AppDetail(
+        label = "$appName settings",
+        icon = packageManager.getApplicationIcon(applicationInfo),
+        intent = Intent(context, PreferencesActivity::class.java)
+    )
+
+    fun findAll(): List<AppDetail> {
+        val i = Intent(Intent.ACTION_MAIN, null).addCategory(Intent.CATEGORY_LAUNCHER)
+
+        return packageManager.queryIntentActivities(i, 0).map {
+            AppDetail(
+                label = it.loadLabel(packageManager).toString(),
+                name = it.activityInfo.packageName,
+                icon = it.activityInfo.loadIcon(packageManager),
+                intent = packageManager.getLaunchIntentForPackage(it.activityInfo.packageName)
+            )
+        }.plus(settingsApp).sortedBy { it.label }
+    }
+}
---
+++ app/src/main/java/com/stepface/mylauncher/PreferencesActivity.kt
@@ -0,0 +1,46 @@
+@file:Suppress("DEPRECATION")
+
+package com.stepface.mylauncher
+
+import android.annotation.SuppressLint
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.preference.PreferenceActivity
+import android.preference.PreferenceFragment
+import android.preference.PreferenceManager
+
+class PreferencesActivity : PreferenceActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
+    @SuppressLint("ApplySharedPref")
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        PreferenceManager.getDefaultSharedPreferences(this)
+            .let {
+                if (!it.getBoolean("needsReload", false)) {
+                    it.edit().putBoolean("needsReload",  true).commit()
+                }
+            }
+
+    }
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fragmentManager.beginTransaction().replace(android.R.id.content, MyPreferenceFragment()).commit()
+    }
+
+    class MyPreferenceFragment : PreferenceFragment() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            addPreferencesFromResource(R.xml.preferences)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+}
